4e1bdd4aaab746483ce477a58886e742aba2f822
Erani0 <76605008+Erani0@users.noreply.github.com>
2024-10-27 15:42:44 +0100

    Update drmon.lua

--- drmon.lua
+++ drmon.lua
@@ -144,8 +144,8 @@ function update()
     -- are we on? regulate the input fludgate to our target field strength
     -- or set it to our saved setting since we are on manual
     if ri.status == "running" then
-      autoOutFlux = ri.fieldDrainRate / (1 - (targetStrength/100) )
-      autoInFlux = ( math.max( 10, ri.generationRate ) / ri.fieldDrainRate / (1 - (targetStrength/100) ))
+      autoInFlux = ri.fieldDrainRate / (1 - (targetStrength/200) )
+      autoOutFlux = (math.max( 10, ri.generationRate) / (ri.temperature / targetTemperature))
       print("Target Input Gate: ".. autoInFlux)
       print("Target Output Gate: ".. autoOutFlux)
       influx.setSignalLowFlow(autoInFlux)
