7a273f90d801412d06809deb23d2786a6343fb53
Erani0 <76605008+Erani0@users.noreply.github.com>
2024-10-27 18:54:31 +0100

    Update drmon.lua

--- drmon.lua
+++ drmon.lua
@@ -12,7 +12,7 @@ local identify = false
 -- please leave things untouched from here on
 os.loadAPI("lib/f")
 
-local version = "4.4"
+local version = "4.5"
 
 -- last performed action
 local action = "None since reboot"
