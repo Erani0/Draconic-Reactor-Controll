21d61089cf8bb198b5550bf6da6623f1c9578e01
Erani0 <76605008+Erani0@users.noreply.github.com>
2024-10-27 18:27:03 +0100

    Update drmon.lua

--- drmon.lua
+++ drmon.lua
@@ -1,6 +1,6 @@
 local reactorSide, igateName, ogateName, monName, oFlow, iFlow, mon, monitor, monX, monY, reactor, outflux, influx, ri, monType, modem, message
 
-local targetStrength = 15
+local targetStrength = 20
 local maxTemperature = 7900
 local safeTemperature = 3000
 local targetTemperature = 7000
