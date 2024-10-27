be79d7c2ec0440c382e527c17965d955edc52b2c
Erani0 <76605008+Erani0@users.noreply.github.com>
2024-10-27 16:16:22 +0100

    Update drmon.lua

--- drmon.lua
+++ drmon.lua
@@ -145,7 +145,7 @@ function update()
     -- or set it to our saved setting since we are on manual
     if ri.status == "running" then
       autoInFlux = ri.fieldDrainRate / (1 - (targetStrength/100) )
-      autoOutFlux = (math.max( 10, ri.generationRate) / (ri.temperature / targetTemperature) / 2)
+      autoOutFlux = (math.max( 10, ri.generationRate) / (ri.temperature / targetTemperature) / 0.5)
       print("Target Input Gate: ".. autoInFlux)
       print("Target Output Gate: ".. autoOutFlux)
       influx.setSignalLowFlow(autoInFlux)
